80f9f22634b1a50fe5d9e4f219426fb52f1f0d0d
kimseungkyu <kimseungkyubestiedu@gmail.com>
2022-05-27 14:33:59 +0900

    feat:programmers greedy making a big number

--- 1_그리디_문제/15_큰수만들기(프로그래머스)/큰수만들기_2.py
+++ 1_그리디_문제/15_큰수만들기(프로그래머스)/큰수만들기_2.py
@@ -2,6 +2,8 @@ data = "1231423"
 k = 4
 tmp_data = []
 answer = []
+f_index = 0
+answer = []
 for i, number in enumerate(data):
     number = int(number)
     tmp_data.append((i, number))
@@ -14,12 +16,20 @@ while k > 0:
         if tmp_max < n:
             tmp_max = n
             tmp_max_i = i
+    # print(tmp_max, tmp_max_i)
     # tmp_max에는 가장 큰 수가 저장되어있음.
-    if tmp_max_i + 1 > len(tmp_data) - k:  # <--이 부분이 문제
-        tmp_data = tmp_data[:tmp_max_i]
-        print(tmp_data)
+    if tmp_max_i >= len(data) - k:  # <--이 부분이 문제
+        for i, number in enumerate(data[:tmp_max_i]):
+            number = int(number)
+            tmp_data = []
+            tmp_data.append((i, number))
+        # print("tmp_max_i초과", tmp_data)
     else:
         answer.append(tmp_max)
-        tmp_data = tmp_data[tmp_max_i + 1 :]
+        for i, number in enumerate(data[tmp_max_i + 1 :]):
+            number = int(number)
+            tmp_data = []
+            tmp_data.append((i, number))
+        # print("tmp_max_i미만", tmp_data)
     k -= 1  # < --이 부분이 문제.
 print(answer)
