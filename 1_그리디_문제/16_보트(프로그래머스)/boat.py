1283260558d65e028c2b275af9d99b947fe02f5e
kimseungkyu <kimseungkyubestiedu@gmail.com>
2022-05-31 22:13:19 +0900

    feat:programmers greedy boat

--- 1_그리디_문제/16_보트(프로그래머스)/boat.py
+++ 1_그리디_문제/16_보트(프로그래머스)/boat.py
@@ -1,19 +1,15 @@
 def solution(people, limit):
     answer = 0
-    people.sort()
-    boat = 0
-    for weight in people:
-        # 현재 보트에 타있는 사람 무게 + 앞으로 들어올 사람의 무게가 한계보다 크다면
-        if boat + weight > limit:
-            # print(boat + weight)
-            answer += 1  # 기존 보트는 떠나고
-            boat = 0  # 새로운 보트를 추가시켜라
-        # 보트에 사람을 태워라
-        boat += weight
-        # print("boat", boat)
-    # 만약 보트에 사람이 있다면 해당 보트도 갯수에 추가시켜라.
-    if boat:
-        answer += 1
+    while people:
+        for weight in people:
+            temp = limit - weight
+            people.remove(weight)
+            temp2 = list(filter(lambda x: x <= temp, people))
+            if temp2:
+                max_data = max(temp2)
+                people.remove(max_data)
+            answer += 1
+            break
 
     return answer
 
